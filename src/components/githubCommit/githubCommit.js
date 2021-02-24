43c2ef0f780c8e0726ab1bff20033482edb81c7e
Vitaliy <vitaliy.polishchuk.ck@gmail.com>
2021-02-24 17:49:41 +0200

    f

--- src/components/githubCommit/githubCommit.js
+++ src/components/githubCommit/githubCommit.js
@@ -9,28 +9,57 @@ const GithubCommit = (props) => {
     const [commit, setCommit] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
-    useEffect( () =>{
-     fetch(
-         `https://api.github.com/search/commits?q=repo:facebook/react+css&page=${page}`,
-         {
-             method: 'GET',
-             headers: new Headers({
-                 Accept: "application/vnd.github.cloak-preview"
-             })
-         }
-     ).then(r => r.json())
-         .then(r => r.commit )
-         .catch(error => console.log(error))
-    },[page])
+    const changeButton = () => {
+        setPage(page + 1)
+    }
 
-     return(
-        <div className='component-github-commit'>
-            {commit.forEach(res => {
-                <li>{res.committer.date}</li>
-            })
+    const showLoading = () => {
+        setIsLoading(true)
+    }
 
+    const hideLoading = () => {
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        showLoading()
+        fetch(
+            `https://api.github.com/search/commits?q=repo:facebook/react+css&page=${page}`,
+            {
+                method: 'GET',
+                headers: new Headers({
+                    Accept: "application/vnd.github.cloak-preview"
+                })
             }
+        ).then(r => r.json())
+            .then(res => {
+                setCommit(res.items)
+                hideLoading()
+            })
+            .catch(error => console.log(error))
+    }, [page])
 
+    return (
+        <div className='component-github-commit'>
+            <div className='home h3 bg-dark p-3 text-center '><NavLink to=''>Home</NavLink></div>
+
+            {
+                isLoading
+                    ? (
+                        <div className='display-4 text-center my-3'>Loading message...</div>
+                    )
+                    : (
+                        <div>
+                            <button className='text-danger border bg-secondary' onClick={changeButton}>Change page</button>
+                            <div className='border bg-light p-5'> {commit.map((res, index) =>
+                                <li key={index}>{res.commit.committer.date}
+                                    <div className='h3 my-3 text-capitalize border-bottom'>{res.commit.message}</div>
+                                </li>
+                            )}
+                            </div>
+                        </div>
+                    )
+            }
         </div>
     )
 }
