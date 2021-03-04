e3856af27f4f1900c5a12a1ebe394b3eba94f448
Vitaliy <vitaliy.polishchuk.ck@gmail.com>
2021-03-04 15:23:59 +0200

    react

--- src/components/forgotPass/forgotPass.js
+++ src/components/forgotPass/forgotPass.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import './forgotPass.css'
-import {NavLink} from "react-router-dom";
-import * as Yup from "yup";
-import {Form, Formik} from "formik";
-
-const ForgotSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
-});
-
 
 const ForgotPass = () => {
-    let initialValues = {
-
-        email: '',
-    };
-    let onSubmit = values => {
-
-        console.log(values);
-    };
     return (
         <div className='container'>
             <div className="form-gap"/>
@@ -31,39 +15,32 @@ const ForgotPass = () => {
                                     <h2 className="text-center">Forgot Password?</h2>
                                     <p>You can reset your password here.</p>
                                     <div className="panel-body">
-                                        <Formik initialValues={initialValues} validationSchema={ForgotSchema}
-                                                onSubmit={onSubmit}>
-                                            {(params) => {
-                                                const {errors, touched} = params;
 
-                                                return (
-                                                    <Form>
+                                        <form>
 
-                                                        <div className="form-group">
-                                                            <div className="input-group">
+                                            <div className="form-group">
+                                                <div className="input-group">
                       <span className="input-group-addon"><i
                           className="glyphicon glyphicon-envelope color-blue"/></span>
-                                                                <input id="email" name="email"
-                                                                       placeholder="Email address"
-                                                                       className="form-control p-4" type="email"
-                                                                       pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                                                                       required/>
-                                                            </div>
-                                                        </div>
-                                                        <div className="form-group">
-                                                            <input name="recover-submit"
-                                                                   className="btn btn-lg btn-primary btn-block"
-                                                                   value="Reset Password"
-                                                                   type="submit"/>
-                                                        </div>
+                                                    <input id="email" name="email"
+                                                           placeholder="Email address"
+                                                           className="form-control p-4" type="email"
+                                                           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                                                           required/>
+                                                </div>
+                                            </div>
+                                            <div className="form-group">
+                                                <input name="recover-submit"
+                                                       className="btn btn-lg btn-primary btn-block"
+                                                       value="Reset Password"
+                                                       type="submit"/>
+                                            </div>
 
-                                                        <input type="hidden" className="hide" name="token" id="token"
-                                                               value=""/>
+                                            <input type="hidden" className="hide" name="token" id="token"
+                                                   value=""/>
 
-                                                    </Form>
-                                                )
-                                            }}
-                                        </Formik>
+                                        </form>
+
                                     </div>
                                 </div>
                             </div>
